10818e5451c4dbd607ed56079930e8337e67c959
Gustavo_Ishizawa_ <guzin.ishizawa@gmail.com>
2024-10-01 15:58:06 -0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 2)
-    host = "https://challenge2024brute.com"  # Seu domínio
+    host = "https://challenge2024brute.com" 
 
     @task
     def load_test(self):
@@ -17,23 +17,23 @@ class WebsiteUser(HttpUser):
         self.client.get("/#")
 
 
-# Configuração para alta carga com múltiplos usuários e frequências
+
 class HeavyLoadUser(HttpUser):
-    wait_time = between(0.001, 0.1)  # Defina o intervalo de tempo de espera entre requisições
+    wait_time = between(0.001, 0.1)  
     host = "https://challenge2024brute.com"
     
     @task
     def high_frequency_requests(self):
-        self.client.get("/")  # Requisição GET na raiz
-        self.client.post("/", json={"data": "high_load"})  # Requisição POST com alta carga
-        self.client.get("/other-endpoint")  # Requisição GET em outro endpoint
+        self.client.get("/") 
+        self.client.post("/", json={"data": "high_load"}) 
+        self.client.get("/other-endpoint")  
     
     def on_start(self):
         print("Iniciando múltiplos tipos de requisições")
 
     @task
     def load_test_heavy(self):
-        for i in range(100000000):  # Número razoável de requisições por loop
+        for i in range(100000000):  
             response = self.client.get("/")
             if response.status_code != 200:
                 print(f"Erro na requisição {i}: {response.status_code}")
